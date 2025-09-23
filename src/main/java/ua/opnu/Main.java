16632465bdf5f7d347dc21ba1a8702cf0380fc29
Agehesha <romashugaev99@gmail.com>
2025-09-23 16:47:54 +0300

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,35 +2,29 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
-        // === Test Student ===
+        // Student example
         Student st = new Student("Ivan", 2);
         st.addCourse("Math");
+        st.addCourse("Physics");
         st.addCourse("Programming");
-
         System.out.println(st.getName() + ": number of courses - " + st.getCourseCount());
-        System.out.println(st.getName() + ": year - " + st.getYear());
+        System.out.println(st.getName() + ": year of study - " + st.getYear());
         System.out.println(st.getName() + ": tuition paid - " + st.getTuition());
 
-        // === Test BankAccount ===
-        BankAccount acc1 = new BankAccount();
-        acc1.name = "Alex";
-        acc1.deposit(1000);
-        acc1.transactionFee = 10;
+        // BankAccount example
+        BankAccount acc1 = new BankAccount("Petro", 1000);
+        BankAccount acc2 = new BankAccount("Oleh", 500);
+        acc1.setTransactionFee(10);
+        acc1.withdraw(200);
+        acc1.transfer(acc2, 300);
+        System.out.println("Petro's balance: " + acc1.getBalance());
+        System.out.println("Oleh's balance: " + acc2.getBalance());
 
-        BankAccount acc2 = new BankAccount();
-        acc2.name = "Maria";
-
-        acc1.transfer(acc2, 200);
-
-        System.out.println(acc1.name + " balance: " + acc1.getBalance());
-        System.out.println(acc2.name + " balance: " + acc2.getBalance());
-
-        // === Test TimeSpan ===
+        // TimeSpan example
         TimeSpan t1 = new TimeSpan(2, 30);
         TimeSpan t2 = new TimeSpan(1, 45);
-
         t1.addTimeSpan(t2);
-        System.out.println("t1 total minutes: " + t1.getTotalMinutes());
-        System.out.println("t1 total hours: " + t1.getTotalHours());
+        System.out.println("Total hours: " + t1.getTotalHours());
+        System.out.println("Total minutes: " + t1.getTotalMinutes());
     }
 }
