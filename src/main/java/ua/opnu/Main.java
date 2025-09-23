a4adb65a8f21cfb3a338302d86c4e0a78e6dfcb5
Agehesha <romashugaev99@gmail.com>
2025-09-23 15:23:30 +0300

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,5 +2,21 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a student object
+        Student st = new Student("Ivan Petrenko", 2);
+
+        // Add some courses
+        st.addCourse("Mathematics");
+        st.addCourse("Programming");
+        st.addCourse("Physics");
+
+        // Print number of courses
+        System.out.println(st.getName() + ": number of courses - " + st.getCourseCount());
+
+        // Print year of study
+        System.out.println(st.getName() + ": year of study - " + st.getYear());
+
+        // Print tuition fee
+        System.out.println(st.getName() + ": tuition paid - " + st.getTuition());
     }
 }
